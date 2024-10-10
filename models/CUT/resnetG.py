bbd0a9eb56742cdd071dc1d6010963b62b62aa6d
Kevin Putra Santoso <92637327+echelon2718@users.noreply.github.com>
2024-10-10 15:38:25 +0700

    Update resnetG.py

--- models/CUT/resnetG.py
+++ models/CUT/resnetG.py
@@ -167,7 +167,23 @@ class ResnetBlock(nn.Module):
         return out
 
 class ResnetGenerator(nn.Module):
+    """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
+
+    We adapt Torch code and idea from Justin Johnson's neural style transfer project(https://github.com/jcjohnson/fast-neural-style)
+    """
+
     def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect', no_antialias=False, no_antialias_up=False, opt=None):
+        """Construct a Resnet-based generator
+
+        Parameters:
+            input_nc (int)      -- the number of channels in input images
+            output_nc (int)     -- the number of channels in output images
+            ngf (int)           -- the number of filters in the last conv layer
+            norm_layer          -- normalization layer
+            use_dropout (bool)  -- if use dropout layers
+            n_blocks (int)      -- the number of ResNet blocks
+            padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
+        """
         assert(n_blocks >= 0)
         super(ResnetGenerator, self).__init__()
         self.opt = opt
@@ -175,98 +191,92 @@ class ResnetGenerator(nn.Module):
             use_bias = norm_layer.func == nn.InstanceNorm2d
         else:
             use_bias = norm_layer == nn.InstanceNorm2d
-        
-        model = [
-            nn.ReflectionPad2d(3),
-            nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
-            norm_layer(ngf),
-            nn.ReLU(True)
-        ]
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+                 norm_layer(ngf),
+                 nn.ReLU(True)]
 
         n_downsampling = 2
-        for i in range(n_downsampling):
+        for i in range(n_downsampling):  # add downsampling layers
             mult = 2 ** i
-            if no_antialias:
-                model += [
-                    nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
-                    norm_layer(ngf * mult * 2),
-                    nn.ReLU(True)
-                ]
+            if(no_antialias):
+                model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                          norm_layer(ngf * mult * 2),
+                          nn.ReLU(True)]
             else:
-                model += [
-                    nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=1, padding=1, bias=use_bias),
-                    norm_layer(ngf * mult * 2),
-                    nn.ReLU(True),
-                    Downsample(ngf * mult * 2)
-                ]
-            
-            mult = 2 ** n_downsampling
+                model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=1, padding=1, bias=use_bias),
+                          norm_layer(ngf * mult * 2),
+                          nn.ReLU(True),
+                          Downsample(ngf * mult * 2)]
 
-            for i in range(n_blocks):
-                model += [
-                    ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)
-                ]
-            
-            for i in range(n_downsampling):
-                mult = 2 ** (n_downsampling - i)
-                if no_antialias_up:
-                    model += [
-                        nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-                        norm_layer(int(ngf * mult / 2)),
-                        nn.ReLU(True)
-                    ]
-                else:
-                    model += [
-                        Upsample(ngf * mult),
-                        nn.Conv2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=1, padding=1, bias=use_bias),
-                        norm_layer(int(ngf * mult / 2)),
-                        nn.ReLU(True)
-                    ]
-            
-            model += [nn.ReflectionPad2d(3)]
-            model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
-            model += [nn.Tanh()]
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):       # add ResNet blocks
 
-            self.model = nn.Sequential(*model)
-        
-    def forward(self, input, layers=[], encode_only=False, mode='all', stop_layer=16):
+            model += [ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+
+        for i in range(n_downsampling):  # add upsampling layers
+            mult = 2 ** (n_downsampling - i)
+            if no_antialias_up:
+                model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                             kernel_size=3, stride=2,
+                                             padding=1, output_padding=1,
+                                             bias=use_bias),
+                          norm_layer(int(ngf * mult / 2)),
+                          nn.ReLU(True)]
+            else:
+                model += [Upsample(ngf * mult),
+                          nn.Conv2d(ngf * mult, int(ngf * mult / 2),
+                                    kernel_size=3, stride=1,
+                                    padding=1,  # output_padding=1,
+                                    bias=use_bias),
+                          norm_layer(int(ngf * mult / 2)),
+                          nn.ReLU(True)]
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input, layers=[], encode_only=False,mode='all',stop_layer=16):
         if -1 in layers:
             layers.append(len(self.model))
         if len(layers) > 0:
             feat = input
             feats = []
-
             for layer_id, layer in enumerate(self.model):
+                # print(layer_id, layer)
                 feat = layer(feat)
                 if layer_id in layers:
+                    # print("%d: adding the output of %s %d" % (layer_id, layer.__class__.__name__, feat.size(1)))
                     feats.append(feat)
-                
                 else:
+                    # print("%d: skipping %s %d" % (layer_id, layer.__class__.__name__, feat.size(1)))
                     pass
-
                 if layer_id == layers[-1] and encode_only:
-                    return feats
-            
-            return feat, feats
-        
+                    # print('encoder only return features')
+                    return feats  # return intermediate features alone; stop in the last layers
+
+            return feat, feats  # return both output and intermediate features
         else:
-            if mode == "encoder":
-                feat = input
+            """Standard forward"""
+            if mode=='encoder':
+                feat=input
                 for layer_id, layer in enumerate(self.model):
                     feat = layer(feat)
-                    if layer_id == stop_layer:
-                        return feat
-            
-            elif mode == "decoder":
-                feat = input
+                    if layer_id ==  stop_layer:
+                        # print('encoder only return features')
+                        return feat  # return intermediate features alone; stop in the last layers
+            elif mode =='decoder':
+                feat=input
                 for layer_id, layer in enumerate(self.model):
+                    
                     if layer_id > stop_layer:
                         feat = layer(feat)
                     else:
                         pass
-                
-                return feat
-
+                        # print('encoder only return features')
+                return feat  # return intermediate features alone; stop in the last layers
             else:
                 fake = self.model(input)
                 return fake
